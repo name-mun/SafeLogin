14651bca8f87f8519b1794c810d16684ac1dacd7
name-mun <tains9153@gmail.com>
2025-04-18 19:58:57 +0900

    [Feat] 회원가입 입력 필드 구현

--- SafeLogin/Souce/Presentation/SignupView.swift
+++ SafeLogin/Souce/Presentation/SignupView.swift
@@ -12,10 +12,33 @@ import Then
 
 final class SignupView: UIView {
     
+    // MARK: - UI Components
+
+    // 타이틀 라벨
     private let titleLabel = UILabel().then {
         $0.text = "회원가입"
         $0.textAlignment = .center
     }
+    
+    // 입력 스택 뷰
+    private let inputStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+        $0.alignment = .center
+        $0.spacing = 12
+    }
+    
+    // 아아디 뷰
+    private let idView = InputView(.id)
+    
+    // 비밀번호 뷰
+    private let passwordView = InputView(.password)
+    
+    // 비밀번호 확인 뷰
+    private let confirmPasswordView = InputView(.confirmPassword)
+    
+    // 닉네임 뷰
+    private let nickNameView = InputView(.nickName)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,18 +49,39 @@ final class SignupView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - 레이아웃 구성
+
     private func setupUI() {
         backgroundColor = .white
         
         [
-            titleLabel
+            titleLabel,
+            inputStackView
         ].forEach {
             addSubview($0)
+        }
+        
+        [
+            idView,
+            passwordView,
+            confirmPasswordView,
+            nickNameView
+        ].forEach {
+            inputStackView.addArrangedSubview($0)
+            $0.snp.makeConstraints {
+                $0.leading.trailing.equalToSuperview()
+            }
         }
         
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide.snp.top).inset(10)
             $0.centerX.equalToSuperview()
+        }
+        
+        inputStackView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.bottom.equalToSuperview().inset(200)
         }
     }
     
