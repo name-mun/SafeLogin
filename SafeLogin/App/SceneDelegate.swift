38c6baa33c95f82ecca4c3f27d7bcc159aadec44
name-mun <tains9153@gmail.com>
2025-04-18 15:49:29 +0900

    Initial Commit

---
+++ SafeLogin/App/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  SafeLogin
+//
+//  Created by 서문가은 on 4/18/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: scene)
+        let navigationController = UINavigationController()
+        self.window = window
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        navigationController.setViewControllers([ViewController()], animated: true)
+    }
+
+
+}
+
---
+++ SafeLogin/Souce/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeLogin
+//
+//  Created by 서문가은 on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
